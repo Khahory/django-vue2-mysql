fea73c8728c219f86e60f34ef46cb8d605bb5895
Khahory <angeld05@live.com>
2022-11-20 16:30:30 -0400

    estoy con mysql ahora

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -95,6 +95,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cardanime_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOTS': 'localhost',
+        'PORT': ''
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
