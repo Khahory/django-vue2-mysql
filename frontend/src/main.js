0848644e845ec46eb8bf9e535764a98d417d275f
Khahory <angeld05@live.com>
2022-11-26 12:59:32 -0400

    frontend added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// imports buefy
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy, {
+    defaultIconPack: 'fas'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/views/AnimeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Anime View</h2>
+</template>
+
+<script>
+export default {
+    name: "AnimeView"
+}
+</script>
+
+<style scoped>
+
+</style>
